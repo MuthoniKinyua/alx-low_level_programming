c810316e9d9b77691c05f733829cd1c74364e866
MuthoniKinyua <joskin074@gmail.com>
2023-03-17 15:16:23 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include <time.h>
 /* more headers goes there*/
 #include <stdio.h>
-/* betty style doc for function main gors there */
+/* betty style doc for function main goes there */
 /**
  * main - Entry point
  *
