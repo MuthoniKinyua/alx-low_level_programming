84625abc55b130f2fb083d7d7285259790f537f5
MuthoniKinyua <joskin074@gmail.com>
2023-03-17 18:22:06 -0700

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	char i;
-	
+
 	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
 	for (i = 'A' ; i <= 'Z' ; i++)
@@ -16,4 +16,5 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
 
