502cdaac69d245676e1f96f8b0c5405592aca84e
MuthoniKinyua <joskin074@gmail.com>
2023-03-19 04:02:31 -0700

    put c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - principal function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multingual puzzle");
+	return (0);
+}
