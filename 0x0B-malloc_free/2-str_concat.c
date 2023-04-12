1266431b36cd4bbafdec398c9c3051ef780aceb5
MuthoniKinyua <joskin074@gmail.com>
2023-04-12 15:59:30 -0700

    free grid

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,26 +1,29 @@
 #include "main.h"
 
 /**
- * *_strdup - returns a pointer to a newly allocated space in memory
- * @str: string
- * Return: pointer to the duplicated string, or NULL
+ * *str_concat - concatenates two strings
+ * @s1: string
+ * @s2: string
+ * Return: pointer or NULL
  */
 
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	char *a;
-	int i, l = 0;
+	int i, concat_i = 0, l = 0;
+	char *concat_s;
 
-	if (str == NULL)
-		return (NULL);
-	for (i = 0 ; str[i] ; i++)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0 ; s1[i] || s2[i] ; i++)
 		l++;
-	a = malloc(sizeof(char) * (l + 1));
-
-	if (a == NULL)
+	concat_s = malloc(sizeof(char) * l);
+	if (concat_s == NULL)
 		return (NULL);
-	for (i = 0 ; str[i] ; i++)
-		a[i] = str[i];
-	a[l] = '\0';
-	return (a);
+	for (i = 0 ; s1[i] ; i++)
+		concat_s[concat_i++] = s1[i];
+	for (i = 0 ; s2[i] ; i++)
+		concat_s[concat_i++] = s2[i];
+	return (concat_s);
 }
