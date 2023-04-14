783f130187bdf692fae970ffaf8bd1f377562cad
MuthoniKinyua <joskin074@gmail.com>
2023-04-13 18:08:47 -0700

    Trust no one

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _print(char *str, int 1);
+void _print(char *str, int size);
 char *mul(char n,char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
-void init(char *str,int 1);
+void init(char *str,int size);
 
 #include <stdio.h>
 #include <stdlib.h>
